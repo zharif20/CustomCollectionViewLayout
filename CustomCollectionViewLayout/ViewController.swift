7e3ecf62a28beeca2cbc5e3bcf89bff2fc7e557e
zharif hadi <zharif.hadi@123rf.com>
2018-09-23 00:02:14 +0800

    Initial Commit

---
+++ CustomCollectionViewLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewLayout
+//
+//  Created by Local on 23/09/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
